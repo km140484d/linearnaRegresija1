222cead85de66e46d5960953e6db19627b7df44f
km140484d <mkonjikovac12@gmail.com>
2019-10-28 11:20:18 +0100

    Uvedeni prediktori sa kombinacijama x1x2, x1x3... x1x2x3...x1x2x3x4x5 i iscrtani na grafiku

--- linearna.py
+++ linearna.py
@@ -9,7 +9,7 @@ df.columns = ['x1', 'x2', 'x3', 'x4', 'x5', 'y']
 df.insert(0, 'one', 1)
 # df = df.sample(frac=1)
 
-predictors = 12
+predictors = 5
 boundary_index = round(df.shape[0] * 0.8)
 
 X = df.iloc[:, 0:6].to_numpy()
@@ -20,15 +20,35 @@ J_arr = []
 theta_arr = []
 pred_arr = []
 
-
-for i in range(0, predictors):
-    if i > 1:
+for i in range(1, 6):
+    print('PREDICTORS ', predictors)
+    if 1 < i < 5:
         for j in range(1, 6):  # first col is all ones
-            # if i == 1:
-            #     for k in range(j+1, 6):
-            #         X = np.c_[X, (X[:, j] * X[:, k])]
-            # else:
-            X = np.c_[X, (X[:, j] ** i)]
+            for k in range(j + 1, 6):
+                if i == 2:
+                    X = np.c_[X, (X[:, j] * X[:, k])]
+                    predictors = predictors + 1
+                    print('x', j, ' x', k)
+                else:
+                    for l in range(k + 1, 6):
+                        if i == 3:
+                            predictors = predictors + 1
+                            print('x', j, ' x', k, 'x', l)
+                            X = np.c_[X, (X[:, j] * X[:, k] * X[:, l])]
+                        else:
+                            for m in range(l + 1, 6):
+                                if i == 4:
+                                    predictors = predictors + 1
+                                    print('x', j, ' x', k, 'x', l, 'x', m)
+                                    X = np.c_[X, (X[:, j] * X[:, k] * X[:, l] * X[:, m])]
+    else:
+        if i == 5:
+            X = np.c_[X, (X[:, 1] * X[:, 2] * X[:, 3] * X[:, 4] * X[:, 5])]
+            predictors = predictors + 1
+            print('x1 x2 x3 x4 x5')
+
+    pred_arr.append(predictors)
+
     X_train, Y_train = X[0:boundary_index], Y[0:boundary_index]
     X_test, Y_test = X[boundary_index:df.shape[0] - 1], Y[boundary_index:df.shape[0] - 1]
 
@@ -36,12 +56,14 @@ for i in range(0, predictors):
     theta_arr.append(theta)
 
     y_guess = X_test.dot(theta)
-    J = (Y - X.dot(theta)).T.dot(Y - X.dot(theta))
+    J = (Y - X.dot(theta)).T.dot(Y - X.dot(theta)) / Y.shape[0]
     J_arr.append(J)
-    pred_arr.append(5+i)
 
 J_min_index = J_arr.index(min(J_arr))
-print(np.min(J_arr)/df.shape[0])
+theta_opt = theta_arr[J_min_index]
+print(pred_arr[J_min_index])
+print(theta_opt)
+print(np.min(J_arr))
 
 plt.plot(pred_arr, J_arr, label='linear')
 plt.legend()
