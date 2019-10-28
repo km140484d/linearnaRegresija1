e53c5348d2cc8bbd95a21958dbcd6e81ffaadab2
km140484d <mkonjikovac12@gmail.com>
2019-10-28 10:29:35 +0100

    Lin2

--- linearna.py
+++ linearna.py
@@ -2,40 +2,49 @@
 
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 df = pd.read_csv('data.csv', header=None)
 df.columns = ['x1', 'x2', 'x3', 'x4', 'x5', 'y']
 df.insert(0, 'one', 1)
-df = df.sample(frac=1)
+# df = df.sample(frac=1)
 
-predictors = 5
+predictors = 12
 boundary_index = round(df.shape[0] * 0.8)
 
 X = df.iloc[:, 0:6].to_numpy()
+
 Y = df['y'].to_numpy()
 
 J_arr = []
+theta_arr = []
+pred_arr = []
+
 
 for i in range(0, predictors):
     if i > 1:
         for j in range(1, 6):  # first col is all ones
+            # if i == 1:
+            #     for k in range(j+1, 6):
+            #         X = np.c_[X, (X[:, j] * X[:, k])]
+            # else:
             X = np.c_[X, (X[:, j] ** i)]
-
-    X_train = X[0:boundary_index]
-    Y_train = Y[0:boundary_index]
-
-    X_test = X[boundary_index:df.shape[0] - 1]
-    Y_test = Y[boundary_index:df.shape[0] - 1]
+    X_train, Y_train = X[0:boundary_index], Y[0:boundary_index]
+    X_test, Y_test = X[boundary_index:df.shape[0] - 1], Y[boundary_index:df.shape[0] - 1]
 
     theta = np.linalg.inv((X_train.T.dot(X_train))).dot(X_train.T).dot(Y_train)
+    theta_arr.append(theta)
 
     y_guess = X_test.dot(theta)
-    print(i)
-    print(y_guess)
-    print(Y_test)
-    # y = np.linspace(Y_test)
     J = (Y - X.dot(theta)).T.dot(Y - X.dot(theta))
-    print(J)
+    J_arr.append(J)
+    pred_arr.append(5+i)
 
+J_min_index = J_arr.index(min(J_arr))
+print(np.min(J_arr)/df.shape[0])
+
+plt.plot(pred_arr, J_arr, label='linear')
+plt.legend()
+plt.show()
 
 # prvi model obican, drugi: x1^2, x2^2..., treci: x1^3, x2^3
