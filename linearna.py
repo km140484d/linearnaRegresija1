17a053a013216091de846a59acd64b932750dd09
km140484d <mkonjikovac12@gmail.com>
2019-10-28 08:58:46 +0100

    Lin1

--- linearna.py
+++ linearna.py
@@ -6,49 +6,36 @@ import numpy as np
 df = pd.read_csv('data.csv', header=None)
 df.columns = ['x1', 'x2', 'x3', 'x4', 'x5', 'y']
 df.insert(0, 'one', 1)
-# df = df.sample(frac=1)
+df = df.sample(frac=1)
 
-predictors = 9
-boundary_index = round(df.shape[0] * 0.9)
+predictors = 5
+boundary_index = round(df.shape[0] * 0.8)
 
 X = df.iloc[:, 0:6].to_numpy()
 Y = df['y'].to_numpy()
 
-ten_per = round(df.shape[0] * 0.1)
+J_arr = []
 
-for i in range(1, predictors):
+for i in range(0, predictors):
     if i > 1:
         for j in range(1, 6):  # first col is all ones
-            a = X[0:5, j] ** i
             X = np.c_[X, (X[:, j] ** i)]
-    # print(X)
+
+    X_train = X[0:boundary_index]
+    Y_train = Y[0:boundary_index]
+
+    X_test = X[boundary_index:df.shape[0] - 1]
+    Y_test = Y[boundary_index:df.shape[0] - 1]
+
+    theta = np.linalg.inv((X_train.T.dot(X_train))).dot(X_train.T).dot(Y_train)
+
+    y_guess = X_test.dot(theta)
     print(i)
-    J_array = []
-    for k in range(0, 4):
-        X_test = X[ten_per*k:ten_per*(k+1)]
-        Y_test = Y[ten_per*k:ten_per*(k+1)]
-        # print(X_test)
+    print(y_guess)
+    print(Y_test)
+    # y = np.linspace(Y_test)
+    J = (Y - X.dot(theta)).T.dot(Y - X.dot(theta))
+    print(J)
 
-        X_train = np.append(X[0:ten_per*k], X[ten_per*(k+1):])
-        Y_train = np.append(Y[0:ten_per*k], Y[ten_per*(k+1):])
-
-        print(X_test)
-        print()
-        # print(Y_train)
-
-    #     theta = np.linalg.inv((X_train.T.dot(X_train))).dot(X_train.T).dot(Y_train)
-    #     # print(theta)
-    #
-    #     y_guess = X_test.dot(theta)
-    #     # print(y_guess)
-    #     # print(Y_test)
-    #     # y = np.linspace(Y_test)
-    #     J = (Y - X.dot(theta)).T.dot(Y - X.dot(theta))
-    #     J_array.append(J)
-    # print(J / len(J_array))
-    # print()
-    # print()
-
-# X = df[['one', 'x1', 'x2', 'x3', 'x4', 'x5']].to_numpy()
 
 # prvi model obican, drugi: x1^2, x2^2..., treci: x1^3, x2^3
