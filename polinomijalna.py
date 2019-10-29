0bcabbe74669b96fea4f268863e69374d3bf60a1
km140484d <mkonjikovac12@gmail.com>
2019-10-29 09:14:18 +0100

    Izracunato W u lokalno ponderisanoj linearnoj regresiji

--- polinomijalna.py
+++ polinomijalna.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 import numpy as np
+import math
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('data.csv', header=None)
@@ -9,6 +10,7 @@ df.columns = ['x1', 'x2', 'x3', 'x4', 'x5', 'y']
 df.insert(0, 'one', 1)
 # df = df.sample(frac=1)
 
+# polynomial
 predictors = 5
 boundary_index = round(df.shape[0] * 0.8)
 
@@ -50,7 +52,7 @@ for i in range(1, 6):
     pred_arr.append(predictors)
 
     X_train, Y_train = X[0:boundary_index], Y[0:boundary_index]
-    X_test, Y_test = X[boundary_index:df.shape[0] - 1], Y[boundary_index:df.shape[0] - 1]
+    X_test, Y_test = X[boundary_index:df.shape[0]], Y[boundary_index:df.shape[0]]
 
     theta = np.linalg.inv((X_train.T.dot(X_train))).dot(X_train.T).dot(Y_train)
     theta_arr.append(theta)
@@ -62,11 +64,48 @@ for i in range(1, 6):
 J_min_index = J_arr.index(min(J_arr))
 theta_opt = theta_arr[J_min_index]
 print(pred_arr[J_min_index])
-print(theta_opt)
 print(np.min(J_arr))
 
-plt.plot(pred_arr, J_arr, label='linear')
-plt.legend()
-plt.show()
+# plt.plot(pred_arr, J_arr, label='polynomial')
+# plt.legend()
+# plt.show()
+
+# grebena regresija
+X = X[:, 0:pred_arr[J_min_index]]
+X_train = X_train[:, 0:pred_arr[J_min_index]]
+X_test = X_test[:, 0:pred_arr[J_min_index]]
+
+lam = np.linspace(0, 0.5, 101)
+J_ridge_arr = []
+theta_ridge_arr = []
+for i in lam:
+    theta = np.linalg.inv((X_train.T.dot(X_train) + i * np.eye(pred_arr[J_min_index]))).dot(X_train.T).dot(Y_train)
+    theta_ridge_arr.append(theta)
+    J = (Y - X.dot(theta)).T.dot(Y - X.dot(theta)) / Y.shape[0]
+    J_ridge_arr.append(J)
+
+J_min_index = J_arr.index(min(J_arr))
+print(lam[J_min_index])
+theta_opt = theta_arr[J_min_index]
+lam_opt = lam[J_min_index]
+
+# plt.plot(lam, J_ridge_arr, label='ridge')
+# plt.legend()
+# plt.show()
+
+tau = np.linspace(0.1, 5, 5)
+xa = np.repeat([X_test[0]], repeats=len(X_train), axis=0)
+for t in tau:
+    for x in X_test:
+        Xi = np.repeat([x], repeats=len(X_train), axis=0)
+        exp = []
+        for i in range(len(X_train)):
+            exp.append(math.e**(-np.linalg.norm(Xi[i] - X_train[i])**2/(2*t**2)))
+        W = np.diag(exp)
+        # theta = np.linalg.inv(X_test.T.dot(W).dot(X_test)).dot(X_test).T.dot(W).dot(Y_test)
+        # print('theta', theta)
+        # print('W', W)
+        exit(0)
+
 
 # prvi model obican, drugi: x1^2, x2^2..., treci: x1^3, x2^3
