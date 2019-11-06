4ad6cd172ecdcc438d5644a30d758a001d9c8a70
km140484d <mkonjikovac12@gmail.com>
2019-11-06 20:17:29 +0100

    Radi i regularizacija, nije trebalo da se koristi optimalan model za nju

--- linearna.py
+++ linearna.py
@@ -4,20 +4,35 @@ import pandas as pd
 import numpy as np
 import math
 import matplotlib.pyplot as plt
+#
+# def predict(matrix):
+#     X = matrix.to_numpy()
+#     X = np.c_[np.ones((len(X), 1)), X]
+#     print(X)
+#     for i in range(2, 3):
+#         if 1 < i < 5:
+#             for j in range(1, 6):  # first col is all ones
+#                 for k in range(j, 6):
+#                     if i == 2:
+#                         X = np.c_[X, (X[:, j] * X[:, k])]
+#                     else:
+#                         for l in range(k, 6):
+#                             X = np.c_[X, (X[:, j] * X[:, k] * X[:, l])]
+#
+#
+#     ;
 
 df = pd.read_csv('data.csv', header=None)
 df.columns = ['x1', 'x2', 'x3', 'x4', 'x5', 'y']
 df.insert(0, 'one', 1)
 df = df.sample(frac=1)
 
-# polinomska regresija
-
-predictors = 6                                  # inicijalan broj prediktora 5 + 1 (kolona sa 1)
 boundary_index = round(df.shape[0] * 0.8)       # uzeto je da je 80% skup za treniranje, a 20% skup za testiranje
-
 X = df.iloc[:, 0:6].to_numpy()
 Y = df['y'].to_numpy()
 
+# polinomijalna regresija
+predictors = 5                                  # inicijalan broj prediktora 5 + 1 (kolona sa 1)
 J_arr, theta_arr, predictors_arr = [], [], []
 
 for i in range(1, 6):
@@ -47,16 +62,16 @@ for i in range(1, 6):
     J_arr.append((Y - X.dot(theta)).T.dot(Y - X.dot(theta)) / Y.shape[0])
 
 J_min_index = J_arr.index(min(J_arr))
-print('Polinomska, min(J):', J_arr[J_min_index])
+print('Polinomijalna, min(J):', J_arr[J_min_index])
 print('predictor_number_opt:', predictors_arr[J_min_index])
+print(predictors_arr[0:J_min_index + 10])
 plt.plot(predictors_arr, J_arr, label='polynomial')
+plt.xlabel('n')
+plt.ylabel('J')
 plt.legend()
 plt.show()
-# podesavanje prediktora, na model koji daje najbolje rezultate u polinomskoj regresiji
-X = X[:, 0:predictors_arr[J_min_index]]
-X_train, X_test = X_train[:, 0:predictors_arr[J_min_index]], X_test[:, 0:predictors_arr[J_min_index]]
 
-# grebena regresija56
+# grebena regresija
 lam = np.linspace(0, 10, 51)
 J_ridge_arr, theta_ridge_arr = [], []
 for l in lam:
@@ -65,14 +80,21 @@ for l in lam:
     J_ridge_arr.append((Y - X.dot(theta)).T.dot(Y - X.dot(theta)) / Y.shape[0])
 
 J_min_index = J_ridge_arr.index(min(J_ridge_arr))
+theta_opt = theta_arr[J_min_index]
 print('Grebena, min(J):', J_ridge_arr[J_min_index])
 print('lambda_opt:', lam[J_min_index])
 plt.plot(lam, J_ridge_arr, label='ridge')
+plt.xlabel('λ')
+plt.ylabel('J')
 plt.legend()
 plt.show()
 
+# podesavanje prediktora, na model koji daje najbolje rezultate u polinomijalnoj regresiji
+X = X[:, 0:predictors_arr[J_min_index]]
+X_train, X_test = X_train[:, 0:predictors_arr[J_min_index]], X_test[:, 0:predictors_arr[J_min_index]]
+
 # lokalno ponderisana
-tau = np.linspace(0.1, 10, 20)
+tau = np.linspace(0.1, 20, 30)
 J_pond_arr = []
 for t in tau:
     J = 0
@@ -89,5 +111,7 @@ J_min_index = J_pond_arr.index(min(J_pond_arr))
 print('Lokalno ponderisana, min(J):', J_pond_arr[J_min_index])
 print('tau_opt:', round(tau[J_min_index], 5))
 plt.plot(tau, J_pond_arr, label='local segmented')
+plt.xlabel('τ')
+plt.ylabel('J')
 plt.legend()
 plt.show()
